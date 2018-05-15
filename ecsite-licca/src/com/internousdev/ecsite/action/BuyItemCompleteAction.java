c5dc2bb29ce4c46cdf07de1fe357818536d61758
licca375 <lica375@icloud.com>
2018-05-15 16:37:49 +0900

    0515

---
+++ ecsite-licca/src/com/internousdev/ecsite/action/BuyItemCompleteAction.java
@@ -0,0 +1,16 @@
+package com.internousdev.ecsite.action;
+import com.opensymphony.xwork2.ActionSupport;
+import java.util.Map;
+import org.apache.struts2.interceptor.SessionAware;
+public class BuyItemCompleteAction extends ActionSupport implements SessionAware {
+public Map<String,Object>session;
+
+public String execute(){
+	return SUCCESS;
+}
+@Override
+public void setSession(Map<String,Object>session){
+	this.session = session;
+
+}
+}
